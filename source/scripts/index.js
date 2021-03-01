f2de6aeb087f194f19731cd24cdc011ddf1728ef
VyglD <VyglD@yandex.ru>
2021-03-02 01:43:28 +0300

    Missing scripts was added

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -12,6 +12,34 @@ const showBlankMessage =
 	toast.info( `Данная страница отсуствует` );
 };
 
+const animate =
+( {timing, draw, duration} ) =>
+{
+	let start = performance.now();
+	
+	const animateFraction =
+	( time ) =>
+	{
+		// eslint-disable-next-line space-in-parens
+		let timeFraction = (time - start) / duration;
+		if ( timeFraction > 1 )
+		{
+			timeFraction = 1;
+		}
+		
+		let progress = timing( timeFraction );
+
+		draw( progress );
+		
+		if ( timeFraction < 1 )
+		{
+			requestAnimationFrame( animateFraction );
+		}
+	};
+	
+	requestAnimationFrame( animateFraction );
+};
+
 initSlider();
 initFeedback( toast );
 
@@ -20,5 +48,43 @@ document.querySelectorAll( `[data-blank-status]` )
 		( node ) =>
 		{
 			node.addEventListener( `click`, showBlankMessage );
+		}
+	);
+
+document.querySelectorAll( `[data-scroll]` )
+	.forEach(
+		( node ) =>
+		{
+			node.addEventListener(
+				`click`,
+				( evt ) =>
+				{
+					evt.preventDefault();
+					
+					const currentYOffset = window.pageYOffset;
+					const scrollOffset =
+						document.querySelector(
+							evt.target.getAttribute( `href` )
+						)
+							.getBoundingClientRect().y;
+					
+					animate(
+						{
+							duration: 500,
+							timing( timeFraction )
+							{
+								return timeFraction;
+							},
+							draw( progress )
+							{
+								scrollTo(
+									0,
+									currentYOffset + scrollOffset * progress
+								);
+							}
+						}
+					);
+				}
+			);
 		}
 	);
